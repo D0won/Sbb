4d033c73e60d522732d599c5cb226539836dcc92
D0won <ldwlee03@gmail.com>
2024-09-09 17:40:18 +0900

    modify AnswerController

--- src/main/java/d0won/sbb/answer/AnswerController.java
+++ src/main/java/d0won/sbb/answer/AnswerController.java
@@ -1,12 +1,15 @@
 package d0won.sbb.answer;
 
+import d0won.sbb.question.Question;
 import d0won.sbb.question.QuestionController;
 import d0won.sbb.question.QuestionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RequestMapping("/answer")
 @RequiredArgsConstructor
@@ -15,6 +18,10 @@ public class AnswerController {
     private final QuestionService questionService;
 
     @PostMapping("/create/{id}")
-    public String createAnswer(Model model, )
+    public String createAnswer(Model model, @PathVariable("id") Integer id,
+                               @RequestParam(value = "content") String content) {
+        Question question= this.questionService.getQuestion(id);
+        // TODO : 답변 저장
+        return String.format("redirect:/question/detail/%s", id);
+    }
 }
- 
